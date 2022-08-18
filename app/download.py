7f9d62286f91d332185e460856b1de937e8ff973
Sreerag M <sreeragibtl@gmail.com>
2022-08-18 15:16:52 +0530

    DEV: Download assets

--- app/download.py
+++ app/download.py
@@ -1,2 +1,7 @@
-def download_assets():
-    pass
+import gdown
+
+
+def download_assets(
+    url: str = "https://drive.google.com/uc?id=1Flw6Z0K2QdRrTn5F-gVt6HdR9TRPiaKy",  # noqa
+):
+    gdown.download(url, quiet=True)
